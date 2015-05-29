ffc04167760a163cdaac64f0a6a0302c41a64b7c
froatsnook <froatsnook@gmail.com>
2015-05-29 20:47:33 +0200

    Update to 0.16.7.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "A flux implementation, packaged for meteor",
-    version: "0.16.6",
+    version: "0.16.7",
     git: "https://github.com/froatsnook/meteor-alt.git",
     name: "froatsnook:alt",
 });
